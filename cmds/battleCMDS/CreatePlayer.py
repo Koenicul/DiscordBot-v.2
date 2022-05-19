547963223d5e7b1ccef935694eb42000bd6578de
Koenicul <koenvaartjes2005@gmail.com>
2022-05-19 21:32:15 +0200

    Deleted test prints

--- cmds/battleCMDS/CreatePlayer.py
+++ cmds/battleCMDS/CreatePlayer.py
@@ -8,16 +8,13 @@ session = Session()
 @commands.command(name="CreatePlayer", help="Create a player")
 async def CreatePlayer(ctx):
     exists = session.query(db.Player).filter(db.Player.id == ctx.author.id).first()
-    print(exists)
     if exists is None:
-        print("Creating player")
-        player = db.Player(ctx.author.id, ctx.author.name, 100, 100, 1, 0, 100, 10, 1, 1)
+        player = db.Player(ctx.author.id, ctx.author.name, 50, 100, 1, 0, 100, 10, 1, 1)
         session.add(player)
         session.commit()
 
         await ctx.send("Player created!")
     else:
-        print(exists.player_id)
         await ctx.send("Player already exists")
 
 def setup(bot):
