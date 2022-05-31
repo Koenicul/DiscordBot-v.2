8a1334cb1ed7704b800dace8c7aa8bae54b30ac5
Koenicul <koenvaartjes2005@gmail.com>
2022-05-31 10:38:54 +0200

    Initial commit

---
+++ cogs/welcome.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+class Start(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send(f"Welcome {member.name}")
+
+def setup(bot):
+    bot.add_cog(Start(bot))
