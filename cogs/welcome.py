c93925bb227ffc5d436f9a9ad921a4fcb7b555ec
Koenicul <koenvaartjes2005@gmail.com>
2022-05-31 12:40:38 +0200

    Changed message

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 class Start(commands.Cog):
     def __init__(self, bot):
@@ -8,7 +9,8 @@ class Start(commands.Cog):
     async def on_member_join(self, member):
         channel = member.guild.system_channel
         if channel is not None:
-            await channel.send(f"Welcome {member.name}")
+            embed = discord.Embed(title=f"Welcome!", description=f"Hello {member.mention}, make sure to read the rules!", color=0x00ff00)
+            await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Start(bot))
